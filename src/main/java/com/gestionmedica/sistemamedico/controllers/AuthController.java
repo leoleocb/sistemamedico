b8672f7c3a462d8181d48f551b6b95fe09056adf
leoleocb <ldch07@outlook.com>
2025-08-07 13:07:25 -0500

    Agregando Controller de login con los html

---
+++ src/main/java/com/gestionmedica/sistemamedico/controllers/AuthController.java
@@ -0,0 +1,32 @@
+package com.gestionmedica.sistemamedico.controllers;
+
+import com.gestionmedica.sistemamedico.dtos.UsuarioDTO;
+import com.gestionmedica.sistemamedico.services.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UsuarioService usuarioService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String mostrarFormularioRegistro(Model model) {
+        model.addAttribute("usuarioDTO", new UsuarioDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String procesarRegistro(@ModelAttribute UsuarioDTO usuarioDTO) {
+        usuarioService.guardarUsuarioConRolPaciente(usuarioDTO);
+        return "redirect:/login?registroExitoso";
+    }
+}
